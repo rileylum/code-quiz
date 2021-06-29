4a35af4fbb3b928f8c93b093eeb6e68dde69c331
Riley Lum <rileylum@adam.com.au>
2021-06-29 17:06:10 +0930

    add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 
+// Create objects for each question
 var questionA = {
     question: "a",
     correctA: 0,
@@ -17,19 +18,27 @@ var questionC = {
     answers: [0, 1, 2, 3]
 };
 
+// Store html elements in variables
 var startGameBtn = document.querySelector("#start-game");
 var timerEl = document.querySelector("#timer");
+
+// initalise variables
 var timeLeft;
 
+// Start the game when button is clicked
 startGameBtn.addEventListener('click', function () {
     timeLeft = 100;
     timerEl.textContent = timeLeft;
     gameTimer = setInterval(countDown, 1000);
 })
 
+// Called from gameTimer Interval
 function countDown() {
+    // Decrement the timer
     timeLeft--;
+    // update value in html
     timerEl.textContent = timeLeft;
+    // stop the interval when time reaches one
     if (timeLeft === 0) {
         clearInterval(gameTimer);
     }
