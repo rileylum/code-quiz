a83bf77819d3b42acbddb854e3b5a0cd3efcaeca
Riley Lum <rileylum@adam.com.au>
2021-06-29 17:43:33 +0930

    create function to display a question on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-
 // Create objects for each question
 var questionA = {
     question: "a",
@@ -7,31 +6,41 @@ var questionA = {
 };
 
 var questionB = {
-    question: "a",
+    question: "b",
     correctA: 0,
     answers: [0, 1, 2, 3]
 };
 
 var questionC = {
-    question: "a",
+    question: "c",
     correctA: 0,
     answers: [0, 1, 2, 3]
 };
 
+// store all questions in array
+var questions = [questionA, questionB, questionC];
+
 // Store html elements in variables
 var startGameBtn = document.querySelector("#start-game");
 var timerEl = document.querySelector("#timer");
+var questionsEl = document.querySelector("#questions");
 
 // initalise variables
 var timeLeft;
+var currentQuestion = 0;
 
 // Start the game when button is clicked
 startGameBtn.addEventListener('click', function () {
+    startTimer();
+    displayQuestion(questions[currentQuestion]);
+})
+
+// set time to 100, update the timer element and then start the countdown
+function startTimer() {
     timeLeft = 100;
     timerEl.textContent = timeLeft;
     gameTimer = setInterval(countDown, 1000);
-})
-
+}
 // Called from gameTimer Interval
 function countDown() {
     // Decrement the timer
@@ -43,3 +52,23 @@ function countDown() {
         clearInterval(gameTimer);
     }
 }
+
+// displays the question which is passed to it in the html
+function displayQuestion(q) {
+    // clear the current content
+    questionsEl.innerHTML = "";
+    // generate html elements from question
+    var questionh2 = document.createElement("h2");
+    var questionList = document.createElement("ul");
+    questionh2.textContent = q.question;
+    for (var i = 0; i < q.answers.length; i++) {
+        var answer = document.createElement("li");
+        answer.textContent = q.answers[i];
+        answer.setAttribute("data-index", i);
+        questionList.appendChild(answer);
+    }
+    // appends html elements to the page
+    questionsEl.appendChild(questionh2);
+    questionsEl.appendChild(questionList);
+}
+
