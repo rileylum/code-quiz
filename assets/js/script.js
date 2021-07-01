826208c6fd7c98ea90c6d0f3abe3ab6d63e4f831
Riley Lum <rileylum@adam.com.au>
2021-07-01 17:33:36 +0930

    change starting screen to be dynamically loaded on pageload and on return from highscore page

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,15 @@ if (!storedScores) {
 };
 
 // Start the game when button is clicked
-startGameBtn.addEventListener('click', function () {
-    startTimer();
-    displayQuestion(questions[currentQuestion]);
+questionsEl.addEventListener('click', function (e) {
+    e.preventDefault();
+    var element = e.target;
+    var endInput = document.querySelector("input");
+
+    if (element.getAttribute("data-type") === "start-game") {
+        startTimer();
+        displayQuestion(questions[currentQuestion]);
+    }
 });
 
 // when an answer is clicked
@@ -61,6 +67,7 @@ questionsEl.addEventListener('click', function (e) {
     }
 });
 
+
 // when submit button is pressed in the game end screen
 questionsEl.addEventListener('click', function (e) {
     e.preventDefault();
@@ -71,6 +78,26 @@ questionsEl.addEventListener('click', function (e) {
         highScores.push({ initials: endInput.value, score: timeLeft });
         localStorage.setItem("highscores", JSON.stringify(highScores));
         showHighScores();
+    }
+});
+
+questionsEl.addEventListener('click', function (e) {
+    e.preventDefault();
+    var element = e.target;
+    var endInput = document.querySelector("input");
+
+    if (element.getAttribute("data-type") === "go-back") {
+        showWelcome();
+    }
+});
+
+questionsEl.addEventListener('click', function (e) {
+    e.preventDefault();
+    var element = e.target;
+    var endInput = document.querySelector("input");
+
+    if (element.getAttribute("data-type") === "clear-score") {
+        clearScores();
     }
 });
 
@@ -151,12 +178,53 @@ function endGame() {
 function showHighScores() {
     // clear screen
     questionsEl.innerHTML = "";
-
+    // create elements
     var highScoreHeading = document.createElement("h1");
     var highScoreList = document.createElement("ol");
-
+    var clearHighScoreBtn = document.createElement("button");
+    var backBtn = document.createElement("button");
+    // edit element content
     highScoreHeading.textContent = "Highscores!";
-
+    backBtn.textContent = "Go Back";
+    backBtn.setAttribute("data-type", "go-back");
+    clearHighScoreBtn.textContent = "Clear Highscores";
+    clearHighScoreBtn.setAttribute("data-type", "clear-score");
+    // create element for each high score and append to list
+    for (var i = 0; i < highScores.length; i++) {
+        var highScoreLi = document.createElement("li");
+        highScoreLi.textContent = highScores[i].initials + " - " + highScores[i].score;
+        highScoreList.appendChild(highScoreLi);
+    }
+    // add elements to page
     questionsEl.appendChild(highScoreHeading);
     questionsEl.appendChild(highScoreList);
+    questionsEl.appendChild(backBtn);
+    questionsEl.appendChild(clearHighScoreBtn);
 }
+
+function showWelcome() {
+    // clear screen
+    questionsEl.innerHTML = "";
+
+    // create elements
+    var welcomeHeading = document.createElement("h1");
+    var welcomeP = document.createElement("p");
+    var welcomeBtn = document.createElement("button");
+
+    // edit element content
+    welcomeHeading.textContent = "JavaScript Coding Quiz";
+    welcomeP.textContent = "Try to answer as many questions as possible during the alloted time, the game ends when you've answered all the questions or the time expires!";
+    welcomeBtn.textContent = "Start Game!";
+    welcomeBtn.setAttribute("data-type", "start-game");
+
+    // add elements to page
+    questionsEl.appendChild(welcomeHeading);
+    questionsEl.appendChild(welcomeP);
+    questionsEl.appendChild(welcomeBtn);
+}
+
+function init() {
+    showWelcome();
+}
+
+init();
